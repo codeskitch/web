62da7325700f46843bb0a6912e4df323ec5140ec
don <don.thrice@gmail.com>
2014-07-25 23:18:34 +0800

    Initial Commit

---
+++ assets/js/codeskitch-main.js
@@ -0,0 +1,81 @@
+﻿function OnSubmit(e) {    
+    Submit(e,'');
+}
+function Submit(e,mailer)
+{
+    e.preventDefault();
+    var btn = $('#btnSubmit');
+    //btn.button('reset')
+    
+    var name = $('input[name="name"]').val();
+    var email = $('input[name="email"]').val();
+    var phone = $('input[name="phone"]').val();
+    var message = $('textarea[name="message"]').val();
+
+    var isValid = true;
+    if (name.replace(' ', '') == "") {
+        isValid = false;
+    }
+
+    var filter = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if (!filter.test(email)) {
+        isValid = false;
+    }
+
+    if (message.replace(' ', '') == "") {
+        isValid = false;
+    }
+
+    if (isValid) {
+        btn.button('loading');
+
+        var param = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "message": message
+        };
+
+        $.ajax({
+            url: 'http://codeskitch.com/mailer'+mailer+'.php',
+            data: param,
+            type: 'POST',
+            dataType: "json",
+            success: function (data) {
+                if (data.status) {
+                    btn.button('complete');
+                    setTimeout(
+                       function () {
+                           $('form').fadeOut();
+                       }
+                        , 500);
+                    setTimeout(function () {
+                        $('#mailsent').fadeIn();
+                        $('form').remove()
+                    }
+                        , 1000)
+                } else {
+                    $(".alert").fadeOut();
+                    setTimeout(function () {
+                        $("#fail-alert").fadeIn()
+                        }
+                        , 1000);
+                }
+            },
+            error: function (data) {                
+                    setTimeout(function () {
+                        $("#fail-alert").fadeIn()
+                    }
+                        , 1000)               
+            }
+        });
+    }
+    else {
+        $(".alert").fadeOut();
+        setTimeout(function () {
+            $("#error-alert").fadeIn()
+        }
+                       , 1000);
+    }
+    return false;
+}
